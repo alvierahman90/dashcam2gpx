f06ab4ace821570537b20be031e99878f8c10438
Akbar Rahman <hi@alv.cx>
2026-03-17 20:52:47 +0000

    fix wrong variable name

--- extract.py
+++ extract.py
@@ -74,7 +74,7 @@ def extract_gps_from_video(video_path, output_gpx, track_name, sample_seconds=5,
     gpx = ET.Element("gpx", version="1.1", creator="dashcam2gps")
     trk = ET.SubElement(gpx, "trk")
     name = ET.SubElement(trk, "name")
-    name.text = gpx_name
+    name.text = track_name
     trkseg = ET.SubElement(trk, "trkseg")
 
     for lat, lon, speed in results:
